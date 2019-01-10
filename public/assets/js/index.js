9172edbff514c6f7046d95fffe2813fe2cd30a28
casekercan <casekercan@gmail.com>
2019-01-09 16:47:28 -0800

    note form created

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,3 +2,31 @@ $("#scrape").on("click", function (event) {
     event.preventDefault();
     window.location = "/scrape";
 });
+
+$(".btn-note").on("click", function (event) {
+    var id = $(this).attr("data-id");
+    window.location = "/articles/" + id;
+});
+
+$("#home").on("click", function (event) {
+    window.location = "/";
+});
+
+$("#addingNote").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).attr("data-id");
+    var body = $("#addNote").val().trim()
+
+    $.ajax({
+        method: "POST",
+        url: "/articles/" + id,
+        data: {
+            body: body
+        }
+    }).then(function (data) {
+        console.log(data);
+        window.location.reload();
+
+    });
+
+})
